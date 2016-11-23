346dc95ae262ffacd017fb66be1594564bca188a
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2016-11-23 15:56:48 +0000

    local_config is no longer declared const. This allows for config settings to be specified at program start (for example input_file_name)

--- include/config/avionics_config.h
+++ include/config/avionics_config.h
@@ -21,10 +21,12 @@ typedef struct avionics_config_t {
     const char* input_file_name;
 
     const char* output_file_name;
+
+    bool output_file_overwrite_enabled;
 } avionics_config_t;
 
 // This should be defined somewhere with the local configuration e.g in main.c
-extern const avionics_config_t local_config;
+extern avionics_config_t local_config;
 
 #ifdef __cplusplus
 }
