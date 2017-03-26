c2c7e83b1d155cb1793e975beba56a760d4008cd
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2017-03-26 16:40:17 +0100

    Updated avionics_component_t

--- include/config/component_state_config.h
+++ include/config/component_state_config.h
@@ -11,19 +11,20 @@ typedef enum {
     avionics_component_adis16405,
     avionics_component_mpu9250,
     avionics_component_ms5611,
-	avionics_component_can_telemetry,
+    avionics_component_can_telemetry,
     avionics_component_world_mag_model,
-	avionics_component_ublox,
-	avionics_component_sd_card,
-	avionics_component_file_telemetry_output,
+    avionics_component_ublox,
+    avionics_component_sd_card,
+    avionics_component_file_telemetry_output,
     avionics_component_state_board_config,
+    avionics_component_state_state_estimate,
     avionics_component_max // This must be last
 } avionics_component_t;
 
 typedef enum {
-	state_ok = 0,
-	state_initializing = 1<<0,
-	state_error = 1<<1,
+    state_ok = 0,
+    state_initializing = 1 << 0,
+    state_error = 1 << 1,
 } avionics_component_state_t;
 
 #ifdef __cplusplus
