ce6443e81dc6cb337903e956cdd9d50266ccdcbe
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2017-04-07 11:25:30 +0100

    Telemetry Packet Tweaks

--- include/config/telemetry_packets.h
+++ include/config/telemetry_packets.h
@@ -80,6 +80,10 @@ typedef struct state_estimate_debug_t {
     float magno_bias[3];
     float accel_ref[3];
     float magno_ref[3];
+    float magno_ref_bias[3];
+    float gyro_sf[3];
+
+    float P[30];
 } state_estimate_debug_t;
 
 #ifdef __cplusplus
